206b63dd4e0ecd5b0f60f452cefb1a4664ef13c4
sheylao <sheypr85@gmail.com>
2019-01-25 01:49:26 -0600

    Adding more changes to code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,3 @@
-
 // Dependencies
 var mysql = require("mysql");
 var inquirer = require("inquirer");
@@ -7,14 +6,14 @@ var inquirer = require("inquirer");
 //Setting mysql connection
 var connection = mysql.createConnection({
     host: "localhost",
-    port:8889,
+    port: 8889,
     user: "root",
     password: "root",
     database: "bamazon_db"
 
 });
 
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) throw err;
     console.log("Connected as id " + connection.threadId);
     queryAllProducts();
@@ -22,10 +21,63 @@ connection.connect(function(err){
 
 
 function queryAllProducts() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      for (var i = 0; i < res.length; i++) {
-        console.log(res[i].id + " | " + res[i].productName+ " | " + res[i].price + " | ");
-      }
-      console.log("-----------------------------------");
+    connection.query("SELECT * FROM products", function (err, res) {
+
+        console.log("")
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].id + " | " + res[i].productName + " | " + res[i].price + " | ");
+        }
+        console.log("-----------------------------------");
+
+        buyingChoice();
     });
-  }
+}
+
+
+function buyingChoice() {
+    inquirer
+        .prompt({
+            name: "select",
+            type: "input",
+            message: "Enter the product number of the Item you want to buy: ",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        })
+        .then(function (answer) {
+            console.log(answer.select);
+
+            var query = "SELECT id,productName,price FROM products WHERE ?";
+            
+            connection.query(query, { id: answer.select }, function (err, res) {
+                console.log("Item: " + res[0].productName + " || Price: " + res[0].price);
+
+                howManyUnits(res[0]);
+
+            });
+
+        });
+
+}
+
+function howManyUnits(product) {
+    inquirer
+        .prompt({
+            name: "units",
+            type: "input",
+            message: "How many " + product.productName + " do you need to buy? ",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false
+            }
+        })
+        .then(function (answer) {
+            console.log(answer.units);
+        })
+
+}
